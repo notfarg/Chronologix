7604ce966139cd5abeb5f57a57c1ffe833081e80
Pamela Payad <payadpamela@gmail.com>
2020-01-27 19:16:36 -0500

    Implemented a simple health bar

--- Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Health.cs
+++ Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Health.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    public Image health;
+    float maxHealth = 100f;
+    public static float healthValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthValue = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        health.fillAmount = healthValue / maxHealth;
+
+        if (Input.GetKeyDown("h"))
+        {
+            healthValue -= 10f;
+        }
     }
 }
