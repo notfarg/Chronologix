36868638358ba4f6a9e573e44a8e1d9b42f31b0c
Pamela Payad <payadpamela@gmail.com>
2020-02-21 02:23:54 -0500

    Added placeholder functionality to Timestopper

--- Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Timestopper.cs
+++ Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Timestopper.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timestopper : MonoBehaviour
 {
+    public Image timestopper;
+    float maxValue = 100f;
+    public static float timestopperValue;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timestopperValue = maxValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timestopperValue -= 0.5f;
+
+        timestopper.fillAmount = timestopperValue / maxValue;
+
+        Debug.Log(timestopperValue);
+
+        if (timestopperValue < 0)
+        {
+            Debug.Log("timstopper Ready");
+        }
     }
 }
