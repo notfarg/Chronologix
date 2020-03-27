1794dc675a5b5d10ca7ad427fbbb27de543a5f4e
Pamela Payad <payadpamela@gmail.com>
2020-03-26 23:38:32 -0400

    Updated Menu Test, added toggleable options menu

--- Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Menu.cs
+++ Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Menu.cs
@@ -2,9 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Menu : MonoBehaviour
 {
+
+    public GameObject soundMenu;
+    public GameObject controlsMenu;
+
+    public GameObject soundMenuContent;
+    public GameObject controlsMenuContent;
+
+    public Toggle optionsToggle;
+    public Toggle soundToggle;
+    public Toggle controlToggle;
+
+    private void Awake()
+    {
+        soundMenu.SetActive(false);
+        controlsMenu.SetActive(false);
+        soundMenuContent.SetActive(false);
+        controlsMenuContent.SetActive(false);
+    }
 
     public void ChangeSceene (string sceneName)
     {
@@ -14,6 +34,48 @@ public class Menu : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("Quit Game");
+    }
+
+    public void OptionsMenu()
+    {
+        if (optionsToggle.isOn)
+        {
+            soundMenu.SetActive(true);
+            controlsMenu.SetActive(true);
+        }
+        else
+        {
+            soundMenu.SetActive(false);
+            controlsMenu.SetActive(false);
+        }
+    }
+
+    public void OpenSound()
+    {
+        if (soundToggle.isOn)
+        {
+            soundMenuContent.SetActive(true);
+            controlsMenuContent.SetActive(false);
+        }
+        else
+        {
+            soundMenuContent.SetActive(false);
+            controlsMenuContent.SetActive(false);
+        }
+    }
+
+    public void OpenControls()
+    {
+        if (controlToggle.isOn)
+        {
+            soundMenuContent.SetActive(false);
+            controlsMenuContent.SetActive(true);
+        } else
+        {
+            soundMenuContent.SetActive(false);
+            controlsMenuContent.SetActive(false);
+        }
+
     }
 
 }
