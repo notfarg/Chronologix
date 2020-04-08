bb51a7742a7c88aacf2a749dcdbea432ba8142cf
Pamela Payad <payadpamela@gmail.com>
2020-04-07 22:46:39 -0400

    Added Title Animation

--- Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Bouncy.cs
+++ Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Bouncy.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Bouncy : MonoBehaviour
 {
+    public float speed;
+    public Vector3 pointA;
+    public Vector3 pointB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Bouncy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
     }
 }
