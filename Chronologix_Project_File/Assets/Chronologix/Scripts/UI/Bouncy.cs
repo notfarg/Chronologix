e3c5e2314bad86ecce638375359d78b9125619c7
Pamela Payad <payadpamela@gmail.com>
2020-04-07 23:24:44 -0400

    Added Progress to New Branch
    Didn't use the updated Master Branch, had to transfer to a new branch that had updated master

---
+++ Chronologix_Project_File/Assets/Chronologix/Scripts/UI/Bouncy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncy : MonoBehaviour
+{
+    public float speed;
+    public Vector3 pointA;
+    public Vector3 pointB;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+    }
+}
